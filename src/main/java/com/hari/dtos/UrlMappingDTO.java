f6dbf28b0d7a4e48517e25917e6005fb1e5360cd
mungsehari <mungse.hari@gmail.com>
2025-01-12 20:32:15 +0530

    add:UrlMappingDto

---
+++ src/main/java/com/hari/dtos/UrlMappingDTO.java
@@ -0,0 +1,16 @@
+package com.hari.dtos;
+
+import java.time.LocalDateTime;
+
+import lombok.Data;
+
+@Data
+public class UrlMappingDTO {
+    private Long id;
+    private String originalUrl;
+    private String shortUrl;
+    private int clickCount;
+    private LocalDateTime createdDate;
+    private String usename;
+
+}
