adba7c720a580001d8c3b97365934f39b5c9b64e
mungsehari <mungse.hari@gmail.com>
2025-01-12 20:35:07 +0530

    add:webConfiguration

---
+++ src/main/java/com/hari/security/WebConfig.java
@@ -0,0 +1,25 @@
+package com.hari.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Value("${frontend.url}")
+    private String frontendUrl;
+
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:5173/")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE",
+                                "OPTIONS")
+                        .allowedHeaders("*").allowCredentials(true).maxAge(3600);
+            }
+        };
+    }
+
+}
