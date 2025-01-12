ceeb9b3b2d62d6dca0b6ac0a75399b8591a6bad2
mungsehari <mungse.hari@gmail.com>
2025-01-12 20:34:26 +0530

    add :clickEventRepository

---
+++ src/main/java/com/hari/repository/ClickEventRepository.java
@@ -0,0 +1,19 @@
+package com.hari.repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.hari.model.ClickEvent;
+import com.hari.model.UrlMapping;
+
+@Repository
+public interface ClickEventRepository extends JpaRepository<ClickEvent, Long> {
+        List<ClickEvent> findByUrlMappingAndClickDateBetween(UrlMapping mapping, LocalDateTime startDate,
+                        LocalDateTime endDate);
+
+        List<ClickEvent> findByUrlMappingInAndClickDateBetween(List<UrlMapping> urlMappings, LocalDateTime startDate,
+                        LocalDateTime endDate);
+}
