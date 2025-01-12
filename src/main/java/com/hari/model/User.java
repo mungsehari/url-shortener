1519da6280f1a6399fa9cd04e9c680dd31571814
mungsehari <mungse.hari@gmail.com>
2025-01-12 20:33:29 +0530

    add : User Entity

---
+++ src/main/java/com/hari/model/User.java
@@ -0,0 +1,23 @@
+package com.hari.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String password;
+    private String username;
+    private String role = "ROLE_USER";
+
+}
